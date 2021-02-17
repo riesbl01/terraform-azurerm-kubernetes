ab27de4c9fee3dbdceb19f804d4dc1ff3d97529b
riesbl01 <69051637+riesbl01@users.noreply.github.com>
2021-02-17 13:29:37 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,11 +10,6 @@ variable "use_service_principal" {
   default     = false
 }
 
-variable "user_assigned_identity_id" {
-  description = "The id of the user assigned identity"
-  type        = string
-}
-
 variable "service_principal_id" {
   description = "Azure Service Principal ID"
   type        = string
